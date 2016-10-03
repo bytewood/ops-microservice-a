c0803ffd2c94bfc0444b60b2a84880f91e3d35d7
Peter Wood <peter@cowboyaliens.com>
2016-10-03 19:53:24 +0200

    Added code to examine whether consul discovery is being enabled

--- src/main/java/spring/ConsulConfiguration.java
+++ src/main/java/spring/ConsulConfiguration.java
@@ -1,14 +1,24 @@
 package spring;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 /**
  * © 2016 org.bytewood
  */
+@Slf4j
 @Profile("consul")
 @Configuration
 @EnableDiscoveryClient
 public class ConsulConfiguration {
+
+    @Bean
+    String discovery() {
+        String s = "CONSUL DISCOVERY ENABLED";
+        log.info(s);
+        return s;
+    }
 }
